e9995d6730c8a3e9d9db621e7c545efb46a22f9f
Rupert <galitrupert1522@gmail.com>
2023-03-07 10:35:41 +0800

    🧬add hover effects

--- login.php
+++ login.php
@@ -118,6 +118,7 @@ h6 span{
   -webkit-transform-style: preserve-3d;
   transform-style: preserve-3d;
   transition: all 600ms ease-out; 
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
 }
 .card-front, .card-back {
   width: 100%;
